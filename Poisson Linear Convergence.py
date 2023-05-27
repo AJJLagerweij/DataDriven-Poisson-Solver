909c1700688eede6773dbddad380513f87d32d3d
Bram Lagerweij <bramlagerweij16@gmail.com>
2023-05-27 15:28:38 +0300

    Updated convergence contourf plots.

--- Poisson Linear Convergence.py
+++ Poisson Linear Convergence.py
@@ -119,7 +119,8 @@ if __name__ == "__main__":
     configuration.optimize(x, material=material, verbose=False, order='Omega0')
     intermediate_J0Omega = np.array(configuration._intermediate_results)
     intermediate_J0Omega = pd.DataFrame({'cost': intermediate_J0Omega[:, 0], 'error': intermediate_J0Omega[:, 1],
-                                         'rot1': intermediate_J0Omega[:, 3], 'rot2': intermediate_J0Omega[:, 4]})
+                                         'rot1': intermediate_J0Omega[:, 3]/domain_length,
+                                         'rot2': intermediate_J0Omega[:, 4]/domain_length})
     intermediate_J0Omega.to_csv("Intermediate_J0Omega.csv")
 
     # Bad initial guess, and verifying how J1Omega converges.
@@ -128,7 +129,8 @@ if __name__ == "__main__":
     configuration.optimize(x, material=material, verbose=False, order='Omega1')
     intermediate_J1Omega = np.array(configuration._intermediate_results)
     intermediate_J1Omega = pd.DataFrame({'cost': intermediate_J1Omega[:, 0], 'error': intermediate_J1Omega[:, 1],
-                                         'rot1': intermediate_J1Omega[:, 3], 'rot2': intermediate_J1Omega[:, 4]})
+                                         'rot1': intermediate_J1Omega[:, 3]/domain_length,
+                                         'rot2': intermediate_J1Omega[:, 4]/domain_length})
     intermediate_J1Omega.to_csv("Intermediate_J1Omega.csv")
 
     # Bad initial guess, and verifying how J0Gamma converges.
@@ -137,7 +139,8 @@ if __name__ == "__main__":
     configuration.optimize(x, material=material, verbose=False, order='Gamma0')
     intermediate_J0Gamma = np.array(configuration._intermediate_results)
     intermediate_J0Gamma = pd.DataFrame({'cost': intermediate_J0Gamma[:, 0], 'error': intermediate_J0Gamma[:, 1],
-                                         'rot1': intermediate_J0Gamma[:, 3], 'rot2': intermediate_J0Gamma[:, 4]})
+                                         'rot1': intermediate_J0Gamma[:, 3]/domain_length,
+                                         'rot2': intermediate_J0Gamma[:, 4]/domain_length})
     intermediate_J0Gamma.to_csv("Intermediate_J0Gamma.csv")
 
     # Bad initial guess, and verifying how J1Gamma converges.
@@ -146,7 +149,8 @@ if __name__ == "__main__":
     configuration.optimize(x, material=material, verbose=False, order='Gamma1')
     intermediate_J1Gamma = np.array(configuration._intermediate_results)
     intermediate_J1Gamma = pd.DataFrame({'cost': intermediate_J1Gamma[:, 0], 'error': intermediate_J1Gamma[:, 1],
-                                         'rot1': intermediate_J1Gamma[:, 3], 'rot2': intermediate_J1Gamma[:, 4]})
+                                         'rot1': intermediate_J1Gamma[:, 3]/domain_length,
+                                         'rot2': intermediate_J1Gamma[:, 4]/domain_length})
     intermediate_J1Gamma.to_csv("Intermediate_J1Gamma.csv")
 
     # Bad initial guess, and verifying how J1Omega_weighted converges.
@@ -155,7 +159,8 @@ if __name__ == "__main__":
     configuration.optimize(x, material=material, verbose=False, order='Omega1_weights')
     intermediate_J1Omega_w = np.array(configuration._intermediate_results)
     intermediate_J1Omega_w = pd.DataFrame({'cost': intermediate_J1Omega_w[:, 0], 'error': intermediate_J1Omega_w[:, 1],
-                                           'rot1': intermediate_J1Omega_w[:, 3], 'rot2': intermediate_J1Omega_w[:, 4]})
+                                           'rot1': intermediate_J1Omega_w[:, 3]/domain_length,
+                                           'rot2': intermediate_J1Omega_w[:, 4]/domain_length})
     intermediate_J1Omega_w.to_csv("Intermediate_J1Omega_weights.csv")
 
     # Plot convergence of Cost vs Error.
@@ -193,54 +198,52 @@ if __name__ == "__main__":
             costJ1Omega_w[i, j] = configuration.error(x, order='Omega1_weights')
             error[i, j] = configuration.compare_to_exact(x, material)
 
+    rotation_domain1 /= domain_length
+    rotation_domain2 /= domain_length
+
     # Plot the error surfaces.
     fig, axs = plt.subplots(2, 3, layout="constrained")
 
     # Cost J^Omega_0
     costH0Omega_surface = axs[0, 0].contourf(rotation_domain1, rotation_domain2, costJ0Omega)
-    axs[0, 0].set_ylabel("Rotation domain 1")
-    axs[0, 0].set_xlabel("Rotation domain 2")
+    axs[0, 0].set_ylabel("Tip displacement domain 1")
+    axs[0, 0].set_xlabel("Tip displacement domain 2")
     fig.colorbar(costH0Omega_surface, ax=axs[0, 0], label="$J^\Omega_0$")
     axs[0, 0].plot(intermediate_J0Omega['rot1'], intermediate_J0Omega['rot2'], 's:', color='C1')
 
     # Cost J^Omega_1
     costH1Omega_surface = axs[0, 1].contourf(rotation_domain1, rotation_domain2, costJ1Omega)
-    axs[0, 1].set_ylabel("Rotation domain 1")
-    axs[0, 1].set_xlabel("Rotation domain 2")
+    axs[0, 1].set_ylabel("Tip displacement domain 1")
+    axs[0, 1].set_xlabel("Tip displacement domain 2")
     fig.colorbar(costH1Omega_surface, ax=axs[0, 1], label="$J^\Omega_1$")
     axs[0, 1].plot(intermediate_J1Omega['rot1'], intermediate_J1Omega['rot2'], 's:', color='C2')
 
     # Cost J^Gamma_0
     costH0Gamma_surface = axs[0, 2].contourf(rotation_domain1, rotation_domain2, costJ0Gamma)
-    axs[0, 2].set_ylabel("Rotation domain 1")
-    axs[0, 2].set_xlabel("Rotation domain 2")
+    axs[0, 2].set_ylabel("Tip displacement domain 1")
+    axs[0, 2].set_xlabel("Tip displacement domain 2")
     fig.colorbar(costH0Gamma_surface, ax=axs[0, 2], label="$J^\Gamma_0")
     axs[0, 2].plot(intermediate_J0Gamma['rot1'], intermediate_J0Gamma['rot2'], 's:', color='C3')
 
     # Cost J^Gamma_1
     costH1Gamma_surface = axs[1, 0].contourf(rotation_domain1, rotation_domain2, costJ1Gamma)
-    axs[1, 0].set_ylabel("Rotation domain 1")
-    axs[1, 0].set_xlabel("Rotation domain 2")
+    axs[1, 0].set_ylabel("Tip displacement domain 1")
+    axs[1, 0].set_xlabel("Tip displacement domain 2")
     fig.colorbar(costH1Gamma_surface, ax=axs[1, 0], label="$J^\Gamma_1$")
     axs[1, 0].plot(intermediate_J1Gamma['rot1'], intermediate_J1Gamma['rot2'], 's:', color='C4')
 
     # Cost J^Omega_1_weighted
     costH1Omega_w_surface = axs[1, 1].contourf(rotation_domain1, rotation_domain2, costJ1Omega_w)
-    axs[1, 1].set_ylabel("Rotation domain 1")
-    axs[1, 1].set_xlabel("Rotation domain 2")
+    axs[1, 1].set_ylabel("Tip displacement domain 1")
+    axs[1, 1].set_xlabel("Tip displacement domain 2")
     fig.colorbar(costH1Omega_w_surface, ax=axs[1, 1], label="$J^\Omega_(4,1)$")
     axs[1, 1].plot(intermediate_J1Omega_w['rot1'], intermediate_J1Omega_w['rot2'], 's:', color='C5')
 
     # Error surface.
     error_surface = axs[1, 2].contourf(rotation_domain1, rotation_domain2, error)
-    axs[1, 2].set_ylabel("Rotation domain 1")
-    axs[1, 2].set_xlabel("Rotation domain 2")
+    axs[1, 2].set_ylabel("Tip displacement domain 1")
+    axs[1, 2].set_xlabel("Tip displacement domain 2")
     fig.colorbar(error_surface, ax=axs[1, 2], label="Error to Exact")
-    axs[1, 2].plot(intermediate_J0Omega['rot1'], intermediate_J0Omega['rot2'], 's:', color='C1')
-    axs[1, 2].plot(intermediate_J1Omega['rot1'], intermediate_J1Omega['rot2'], 's:', color='C2')
-    axs[1, 2].plot(intermediate_J0Gamma['rot1'], intermediate_J0Gamma['rot2'], 's:', color='C3')
-    axs[1, 2].plot(intermediate_J1Gamma['rot1'], intermediate_J1Gamma['rot2'], 's:', color='C4')
-    axs[1, 2].plot(intermediate_J1Omega_w['rot1'], intermediate_J1Omega_w['rot2'], 's:', color='C5')
 
     # Store the surface plots.
     grid_rot1, grid_rot2 = np.meshgrid(rotation_domain1, rotation_domain2)
