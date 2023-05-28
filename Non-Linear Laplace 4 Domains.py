9b416457982e94212baca70253c9d1b105b3320e
Bram Lagerweij <bramlagerweij16@gmail.com>
2023-05-28 13:58:10 +0300

    Update example to have a length of 1000 mm.

---
+++ Non-Linear Laplace 4 Domains.py
@@ -0,0 +1,179 @@
+r"""
+The Laplace example.
+
+The problem solved is the non-linear Laplace equation:
+
+.. math::
+    \div( K [\nabla u]) = 0 \quad 0 \leq x \leq 1000\\
+
+    u(0) = 0\\
+
+    u(1000) = 0
+
+which in this case has a linear conductivity :math:`K=1\nabla u`, and could be solved with the linear non-homogeneous
+solver available in the ODE solver branch of this git. Nevertheless, here it is assumed that the conductivity is not a
+constant, and that it is not known that the actual material is linear.
+
+Our dataset does not contain the solution to this problem. However, it contains the solution for a similar particular
+solution. With Frankenstein's algorithm, we cut the solution in the database in parts and reassemble it. According to
+appendix A of the dissertation, the following solutions for the domain should be considered:
+
+.. math::
+    u_d(x) = u_{p_d}(x-t_{d}) + \bar{u}_d
+
+where :math:`b_d` is an unknown constant. And :math:`u_{p_d}(x)` is the displacement of a patch that satisfies:
+
+.. math::
+    g_{p_d}(x-t_{d}) = \tilde{g}(x) \quad \forall x\in\mathcal{D}_d
+
+where :math:`t_d` is the translation of the patch to global coordinates. The unknowns in this problem are: the patch
+for each domain (discrete), the coordinate translation :math:`t_d`, and rigid temperature addition :math:`\bar{u}_d`.
+In contrast to the ODE solver one might not find the exact solution when minimizing the cost function, hence from all
+configurations the best configuration needs to be selected. The best configuration is the one with the lowest cost.
+
+Bram van der Heijden
+Mechanics of Composites for Energy and Mobility
+KAUST
+2023
+"""
+
+# Importing required modules.
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from functools import partial
+import pandas as pd
+
+# Importing my own scripts.
+from configuration import ConfigurationDatabase
+from problem import Homogeneous
+from test import Laplace_Dirichlet_Dirichlet
+from patch import PatchDatabase
+from constitutive import LinearMaterial
+
+# Setup basic plotting properties.
+plt.close('all')
+
+
+def export_results(configurations, domain_length, problem_length, material):
+    results = pd.DataFrame(columns=['rot1', 'rot2', 'J0Omega', 'J1Omega', 'J0Gmega', 'J1Gmega', 'J1Omega_w', 'error'])
+
+    for num, configuration in enumerate(configurations.database['configuration']):
+        results = pd.concat([results, configuration_details(configuration, domain_length, problem_length, material)])
+
+    return results
+
+
+def configuration_details(configuration, domain_length, problem_length, material):
+    # Calculate slope domain 1 and domain two.
+    x = np.array([domain_length, problem_length - domain_length])
+    ud = configuration.domain_primal(x)
+    slope1 = ud[0, 0]/domain_length
+    slope2 = -ud[1, 1]/domain_length
+
+    x = np.linspace(0, problem_length, 1001)
+    J0Omega = configuration.error(x, order='Omega0')
+    J1Omega = configuration.error(x, order='Omega1')
+    J0Gamma = configuration.error(x, order='Gamma0')
+    J1Gamma = configuration.error(x, order='Gamma1')
+    J1Omega_w = configuration.error(x, order='Omega1_weights')
+    error = configuration.compare_to_exact(x, material)
+    results = pd.DataFrame({'rot1': [slope1], 'rot2': [slope2], 'J0Omega': [J0Omega], 'J1Omega': [J1Omega],
+                            'J0Gamma': [J0Gamma], 'J1Gamma': [J1Gamma], 'J1Omega_w': [J1Omega_w], 'error': [error]})
+    return results
+
+
+if __name__ == "__main__":
+    # Whether to use parallel optimization or not.
+    parallel = False
+
+    # Problem definition.
+    problem_length = 1000.  # Length of the problem in mm.
+    problem_a = 0.  # Left boundary value in degreeC.
+    problem_b = 0.  # Right boundary value in degreeC.
+    domain_num = 2  # Amount subdomains.
+    domain_length = 525  # Length of the subdomains in mm.
+    problem = Homogeneous(problem_length, problem_a, problem_b, domain_length, domain_num)
+    # problem.plot()
+
+    # Material definition, required for the test, and verification of the exact solution.
+    material = LinearMaterial(1000)  # Constant conductivity in W mm / degC
+
+    # Perform test according to the following test matrix.
+    specimen_length = 1000.  # Specimen length in mm.
+    specimen_dx = 0.1  # mm discretization step size (measurement spacial resolution)
+    rhs = lambda x: 0.*x  # rhs in test setup.
+    b_list = [0.55, 0.9523, 1.1429, 2.2857]
+
+    # Create patch database by looping over all tests.
+    database = PatchDatabase()
+    for b in b_list:
+        test = Laplace_Dirichlet_Dirichlet(specimen_length, specimen_dx, 0., b, rhs, material)
+        database.add_test(test)
+    database.mirror()
+    database.plot()
+
+    # Either create a configurations-database from patch admissibility or from loading previous simulation results.
+    x = np.linspace(0, problem_length, 1001)  # Spatial discretization in mm.
+
+    # Either create a configurations-database from patch admissibility or from loading previous simulation results.
+    configurations = ConfigurationDatabase.create_from_problem_patches(problem, database)  # From patch admissibility.
+    configurations.optimize(x, order='Omega0', parallel=parallel)
+    configurations.compare_to_exact(x, material, parallel=parallel)
+    configurations.sort('error')
+    # configurations.plot(x, material, max_images=5)
+
+    # plot and export results.
+    results = export_results(configurations, domain_length, problem_length, material)
+    results.to_csv('Scatter_cost_error.csv')
+
+    # Plot the error pointss.
+    fig, axs = plt.subplots(2, 3, layout="constrained", sharex=True, sharey=True)
+
+    # Cost J^Omega_0
+    costH0Omega_points = axs[0, 0].scatter(results.rot1, results.rot2, c=results.J0Gamma, lw=0)
+    axs[0, 0].set_ylabel("Slope domain 1")
+    axs[0, 0].set_xlabel("Slope domain 2")
+    fig.colorbar(costH0Omega_points, ax=axs[0, 0], label="$J^\Omega_0$")
+
+    # Cost J^Omega_1
+    costH1Omega_points = axs[0, 1].scatter(results.rot1, results.rot2, c=results.J1Omega, lw=0)
+    axs[0, 1].set_ylabel("Slope domain 1")
+    axs[0, 1].set_xlabel("Slope domain 2")
+    fig.colorbar(costH1Omega_points, ax=axs[0, 1], label="$J^\Omega_1$")
+
+    # Cost J^Gamma_0
+    costH0Gamma_points = axs[0, 2].scatter(results.rot1, results.rot2, c=results.J0Gamma, lw=0)
+    axs[0, 2].set_ylabel("Slope domain 1")
+    axs[0, 2].set_xlabel("Slope domain 2")
+    fig.colorbar(costH0Gamma_points, ax=axs[0, 2], label="$J^\Gamma_0")
+
+    # Cost J^Gamma_1
+    costH1Gamma_points = axs[1, 0].scatter(results.rot1, results.rot2, c=results.J1Gamma, lw=0)
+    axs[1, 0].set_ylabel("Slope domain 1")
+    axs[1, 0].set_xlabel("Slope domain 2")
+    fig.colorbar(costH1Gamma_points, ax=axs[1, 0], label="$J^\Gamma_1$")
+
+    # Cost J^Omega_1_weighted
+    costH1Omega_w_points = axs[1, 1].scatter(results.rot1, results.rot2, c=results.J1Omega_w, lw=0)
+    axs[1, 1].set_ylabel("Slope domain 1")
+    axs[1, 1].set_xlabel("Slope domain 2")
+    fig.colorbar(costH1Omega_w_points, ax=axs[1, 1], label="$J^\Omega_(4,1)$")
+
+    # Error points.
+    error_points = axs[1, 2].scatter(results.rot1, results.rot2, c=results.error, lw=0)
+    axs[1, 2].set_ylabel("Slope domain 1")
+    axs[1, 2].set_xlabel("Slope domain 2")
+    fig.colorbar(error_points, ax=axs[1, 2], label="$Error to exact$")
+
+    # Look at the convergence, compare distance to exact solution and overlapping error.
+    plt.figure()
+    plt.xlabel("Cost $J$")
+    plt.ylabel("Distance to Exact Solution $e$")
+    plt.scatter(results.error, results.J0Omega, label='$J^\Omega_0$')
+    plt.scatter(results.error, results.J1Omega, label='$J^\Omega_1$')
+    plt.scatter(results.error, results.J0Gamma, label='$J^\Gamma_0$')
+    plt.scatter(results.error, results.J1Gamma, label='$J^\Gamma_1$')
+    plt.scatter(results.error, results.J1Omega_w, label='$J^\Omega_{4,1}$')
+    plt.legend()
+    plt.show()
--- example.py
+++ example.py
@@ -1,21 +1,56 @@
 r"""
-The beam example.
+The potentially non-linear Poisson example.
 
-The problem solved for is a 1000 mm long simply supported beam. The supports are located as 0 and 250 mm. A load of
-1 N is applied at the end of the beam. It is discretized into 3 domains, each 400 mm long with 100 mm overlap.
+The problem solved is the non-linear Poisson equation:
 
-The database consists of patches obtained from a several simply supported beams. The patches were harvested are 400mm
-long and had a 100 mm overlap. The exact solution is not in the database.
+.. math::
+    \div( K [\nabla u]) = \tilde{g}(x) \quad 0 \leq x \leq 1000\\
+
+    u(0) = 0\\
+
+    u(1000) = 0
+
+which in this case has a linear conductivity :math:`K=1\nabla u`, and could be solved with the linear non-homogeneous
+solver available in the ODE solver branch of this git. Nevertheless, here it is assumed that the conductivity is not a
+constant, and that it is not known that the actual material is linear.
+
+Heating is applied as a hat function.
+
+.. math::
+    g(x) =
+    \begin{cases}
+        0 & 0 \leq x < 400
+        1 & 400 \leq x \leq 600
+        0 & 600 < x \leq 1000
+    \end{cases}
+
+Our dataset does not contain the solution to this problem. However, it contains the solution for a similar particular
+solution. With Frankenstein's algorithm, we cut the solution in the database in parts and reassemble it. According to
+appendix A of the dissertation, the following solutions for the domain should be considered:
+
+.. math::
+    u_d(x) = u_{p_d}(x-t_{d}) + \bar{u}_d
+
+where :math:`b_d` is an unknown constant. And :math:`u_{p_d}(x)` is the displacement of a patch that satisfies:
+
+.. math::
+    g_{p_d}(x-t_{d}) = \tilde{g}(x) \quad \forall x\in\mathcal{D}_d
+
+where :math:`t_d` is the translation of the patch to global coordinates. The unknowns in this problem are: the patch
+for each domain (discrete), the coordinate translation :math:`t_d`, and rigid temperature addition :math:`\bar{u}_d`.
+In contrast to the ODE solver one might not find the exact solution when minimizing the cost function, hence from all
+configurations the best configuration needs to be selected. The best configuration is the one with the lowest cost.
 
 Bram Lagerweij
 Mechanics of Composites for Energy and Mobility
 KAUST
-2021
+2023
 """
 
 # Importing required modules.
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from functools import partial
 
 # Importing my own scripts.
@@ -45,46 +80,45 @@ def rhs_hats(hats, x):
 
 if __name__ == "__main__":
     # Run settings.
-    parallel = True
+    parallel = False
 
     # Problem definition.
-    problem_length = 1.
-    problem_h = 0.2
-    domain_num = 4
-    a = 0.
-    b = -0.05
-    # domain_length = 0.525
-    domain_length = 0.2875  # Length of the subdomains
-    # domain_length = 0.16875
-    problem = Homogeneous(problem_length, a, b, domain_length, domain_num)
+    problem_length = 1000.  # Length of the problem in mm.
+    problem_h = 200.  # Width of the hat function in mm.
+    problem_rhs = 0.0  # Right hand side heating in W / mm.
+    problem_a = 0.00  # Left boundary value in degreeC.
+    problem_b = -0.  # Right boundary value in degreeC.
+    domain_num = 4  # 16 # Amount subdomains.
+    domain_length = 287.5  # 109.375 # Length of the subdomains in mm.
+    problem = Hat(problem_length, problem_h, problem_rhs, problem_a, problem_b, domain_length, domain_num)
+    # problem.plot()
 
     # Material definition.
-    material = LinearMaterial(1)
+    material = LinearMaterial(1000)  # Constant conductivity in W mm / degC
 
     # Create empty database.
     database = PatchDatabase()
 
     # Perform test according to the following test matrix.
-    specimen_length = [1]  # specimen length.
+    specimen_length = [1000.]  # Specimen length in mm.
     rhs_list = [
-                # partial(rhs_hats, [(0.40, 0.60, 1.00)]),  # Exactly the problem, and thus also the exact solution.
-                # partial(rhs_hats, [(0.00, 0.60, 1.00)]),  # Smallest, small, medium, large and largest database.
-                # partial(rhs_hats, [(0.00, 0.40, 1.00)]),  # Smallest, small, medium, large and largest database.
-                # partial(rhs_hats, [(0.40, 1.00, 1.00)]),  # Smallest, small, medium, large and largest database.
-                # partial(rhs_hats, [(0.60, 1.00, 1.00)]),  # Smallest, small, medium, large and largest database.
-                partial(rhs_hats, [(0.30, 0.50, 1.00)]),  # Small, medium, large and largest database.
-                partial(rhs_hats, [(0.50, 0.70, 1.00)]),  # Small, medium, large and largest database.
-                # partial(rhs_hats, [(0.30, 0.70, 0.50)]),  # Medium, large and largest database.
-                # partial(rhs_hats, [(0.45, 0.55, 2.00)]),  # Medium, large and largest database.
-                # partial(rhs_hats, [(0.00, 0.40, 1.00), (0.80, 1.00, 1.00)]),  # Large and largest database.
-                # partial(rhs_hats, [(0.00, 0.40, 1.00), (0.80, 1.00, 0.50)]),  # Large and largest database.
-                # partial(rhs_hats, [(0.00, 0.20, 1.00), (0.40, 0.60, 1.00), (0.80, 1.00, 1.00)]),  # Largest database.
-                # partial(rhs_hats, [(0.00, 0.20, 0.50), (0.40, 0.60, 1.00), (0.80, 1.00, 0.50)]),  # Largest database.
+                # partial(rhs_hats, [(400, 600, 0.2)]),  # Exactly the problem, and thus also the exact solution.
+                partial(rhs_hats, [(  0,  600, 0.20)]),  # Smallest, small, medium, large and largest database.
+                partial(rhs_hats, [(  0,  400, 0.20)]),  # Smallest, small, medium, large and largest database.
+                partial(rhs_hats, [(400, 1000, 0.20)]),  # Smallest, small, medium, large and largest database.
+                partial(rhs_hats, [(600, 1000, 0.20)]),  # Smallest, small, medium, large and largest database.
+                # partial(rhs_hats, [(300,  500, 0.20)]),  # Small, medium, large and largest database.
+                # partial(rhs_hats, [(500,  700, 0.20)]),  # Small, medium, large and largest database.
+                # partial(rhs_hats, [(300,  700, 0.10)]),  # Medium, large and largest database.
+                # partial(rhs_hats, [(450,  555, 0.40)]),  # Medium, large and largest database.
+                # partial(rhs_hats, [(  0,  400, 0.20), (0.80, 1.00, 1.00)]),  # Large and largest database.
+                # partial(rhs_hats, [(  0,  400, 0.20), (0.80, 1.00, 0.50)]),  # Large and largest database.
+                # partial(rhs_hats, [(  0,  200, 0.20), (0.40, 0.60, 1.00), (0.80, 1.00, 1.00)]),  # Largest database.
+                # partial(rhs_hats, [(  0,  200, 0.10), (0.40, 0.60, 1.00), (0.80, 1.00, 0.50)]),  # Largest database.
                 ]  # Potential rhs equations
 
     # Perform the testing and add the result to the database.
-    x = np.linspace(0, 1, 1001)
-    specimen_dx = x[1]  # mm discretization step size (measurement spacial resolution)
+    specimen_dx = 0.1  # mm discretization step size (measurement spacial resolution)
     for length in specimen_length:
         for rhs in rhs_list:
             test = Laplace_Dirichlet_Dirichlet(length, specimen_dx, 0, 0, rhs, material)
@@ -100,41 +134,41 @@ if __name__ == "__main__":
     # configurations = ConfigurationDatabase.create_from_load(f'{name}.pkl.gz')  # Load previous simulation results.
 
     # Configurations are evaluated over at the following locations.
-    x = np.linspace(0, 1, 1001)
+    x = np.linspace(0, problem_length, 1001)
 
     # Perform calculations on the database.
     print(f'{configurations.num_configurations()} are in this database')
-    configurations.optimize(x, parallel=parallel)
+    configurations.optimize(x, order='Omega1_weights', parallel=parallel)
     configurations.compare_to_exact(x, material, parallel=parallel)
+    configurations.database.plot.scatter('error', 'error_to_exact')
     configurations.save(f'{name}.pkl.gz')
 
-    # Get the best configuration in DD-error.
-    configurations.sort('error')
-    config = configurations.database.iloc[0, 0]
-    config.plot(x, material=material)
-
-    # Get the best configuration in distance to the exact solution.
-    configurations.sort('error_to_exact')
-    config = configurations.database.iloc[0, 0]
-    config.plot(x, material=material)
-
-    # Compare the two error norms.
-    configurations.error(x, parallel=parallel)
-    configurations.sort('error')
-    configurations.database.plot.scatter('error', 'error_to_exact')
-    plt.show()
-
-    # import pandas as pd
+    # # Get the best configuration in DD-error.
+    # configurations.sort('error')
     # config = configurations.database.iloc[0, 0]
-    # x = np.linspace(0, 1, 101)
+    # config.plot(x, material=material)
+
+    # # Get the best configuration in distance to the exact solution.
+    # configurations.sort('error_to_exact')
+    # config = configurations.database.iloc[0, 0]
+    # config.plot(x, material=material)
+    #
+    # # Compare the two error norms.
+    # configurations.error(x, parallel=parallel)
+    # configurations.sort('error')
+    # configurations.database.plot.scatter('error', 'error_to_exact')
+    # plt.show()
+
+    # config = configurations.database.iloc[0, 0]
+    # x = np.linspace(0, problem_length, 1001)
     # config.plot(x, material=material)
     # print('e= ', config.error(x))
-    # # u = config.domain_primal(x)
-    # # rhs = config.domain_rhs(x)
-    # # collection = {'x': x}
-    # # for d in range(domain_num):
-    # #     collection[f"u{d + 1}"] = u[d]
-    # #     collection[f"rhs{d + 1}"] = rhs[d]
-    # # data = pd.DataFrame(collection)
-    # # data.to_csv(f"{name}.csv")
+    # u = config.domain_primal(x)
+    # rhs = config.domain_rhs(x)
+    # collection = {'x': x}
+    # for d in range(domain_num):
+    #     collection[f"u{d + 1}"] = u[d]
+    #     collection[f"rhs{d + 1}"] = rhs[d]
+    # data = pd.DataFrame(collection)
+    # data.to_csv(f"{name}.csv")
     # plt.show()
